cb828e6bf54b1f61cb9ef08afdc5c10d3f928ada
Rodrigo <rodrigo_ladaga@hotmail.com>
2024-11-10 12:12:46 -0300

    Nueva implementacion EvaluadorMano
    Co-authored-by: Agustin De Toffoli <adetoffoli@fi.uba.ar>
    Co-authored-by: Anibal Alvarez <analvarez@fi.uba.ar>
    Co-authored by: Tomas Apud Mora <tapud@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/EvaluadorMano.java
+++ src/main/java/edu/fiuba/algo3/modelo/EvaluadorMano.java
@@ -1,56 +1,56 @@
 package edu.fiuba.algo3.modelo;
 
 import java.util.ArrayList;
-
-enum Manos {
-    CARTA_ALTA(5, 1),
-    PAR(10, 2),
-    DOBLE_PAR(20, 2),
-    TRIO(30, 3),
-    ESCALERA(30, 4),
-    COLOR(35, 4),
-    FULL_HOUSE(40, 4),
-    POKER(60, 7),
-    ESCALERA_COLOR(100, 8),
-    ESCALERA_REAL(100, 8);
-
-    private final int puntos;
-    private final int multiplicador;
-
-    Manos(int puntos, int multiplicador) {
-        this.puntos = puntos;
-        this.multiplicador = multiplicador;
-    }
-
-    public int puntuacion() {
-        return puntos;
-    }
-
-    public int multiplicador() {
-        return multiplicador;
-    }
-
-}
+import java.util.List;
+import edu.fiuba.algo3.modelo.tipo_de_mano.*;
 
 
 public class EvaluadorMano {
+    private final List<TipoDeMano> tiposDeMano;
 
-    public EvaluadorMano(){};
-
-    public int evaluar(ArrayList<Carta> mano){
-        int valorBase = 0;
-
-       for (Carta carta : mano) {
-           valorBase += carta.obtenerPuntuacion();
-       }
-
-
-
-       return valorBase;
+    public EvaluadorMano() {
+        tiposDeMano = new ArrayList<TipoDeMano>();
+        tiposDeMano.add(new EscaleraReal());
+        tiposDeMano.add(new EscaleraColor());
+        tiposDeMano.add(new Poker());
+        tiposDeMano.add(new FullHouse());
+        tiposDeMano.add(new Color());
+        tiposDeMano.add(new Escalera());
+        tiposDeMano.add(new Trio());
+        tiposDeMano.add(new DoblePar());
+        tiposDeMano.add(new Par());
+        tiposDeMano.add(new CartaAlta());
     }
 
+    private int calcularValorCartas(ArrayList<Carta> mano) {
+        int valorBase = 0;
 
+        for (Carta carta : mano) {
+            valorBase += carta.obtenerPuntuacion();
+        }
+
+        return valorBase;
+    }
+
+    private int calcularPuntuacionTotal(int valorBase, TipoDeMano tipo) {
+        int puntosPorMano = tipo.calcularPuntos();
+        int multiplicador = tipo.calcularMultiplicador();
+        return (valorBase + puntosPorMano) * multiplicador;
+    }
+
+    public int evaluar(ArrayList<Carta> mano) {
+
+        int valorBase = calcularValorCartas(mano);
+
+        for (TipoDeMano tipo : tiposDeMano) {
+            if (tipo.esValida(mano)) {
+                return calcularPuntuacionTotal(valorBase, tipo);
+            }
+        }
+        return 0;
+    }
 }
+
 
 
 
