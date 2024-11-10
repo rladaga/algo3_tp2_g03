a9345e27649075d8be631490f6bbeb4dd2eeb485
Rodrigo <rodrigo_ladaga@hotmail.com>
2024-11-10 12:11:20 -0300

    Sacamos el enum de aca para hacerlo publico
    Co-authored-by: Agustin De Toffoli <adetoffoli@fi.uba.ar>
    Co-authored-by: Anibal Alvarez <analvarez@fi.uba.ar>
    Co-authored by: Tomas Apud Mora <tapud@fi.uba.ar>

--- src/main/java/edu/fiuba/algo3/modelo/Carta.java
+++ src/main/java/edu/fiuba/algo3/modelo/Carta.java
@@ -1,48 +1,8 @@
 package edu.fiuba.algo3.modelo;
 
-enum Palo {
-    PICA,DIAMANTE,CORAZON,TREBOL;
-
-}
-
-enum Valor {
-    AS("A", 11),
-    DOS("2", 2),
-    TRES("3", 3),
-    CUATRO("4", 4),
-    CINCO("5", 5),
-    SEIS("6", 6),
-    SIETE("7", 7),
-    OCHO("8", 8),
-    NUEVE("9", 9),
-    DIEZ("10", 10),
-    JOTA("J", 10),
-    REINA("Q", 10),
-    REY("K", 10);
-
-    private final String numero;
-    private final int puntoBase;
-
-    Valor(String numero, int puntoBase) {
-        this.numero = numero;
-        this.puntoBase = puntoBase;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public int getPuntoBase() {
-        return puntoBase;
-    }
-}
-
-
 public class Carta {
     private Valor valor;
     private Palo palo;
-
-
 
     public Carta(Palo palo, Valor valor) {
         this.valor = valor;
@@ -59,6 +19,18 @@ public class Carta {
 
     public int obtenerPuntuacion() {
         return this.valor.getPuntoBase();
+    }
+
+    public int obtenerPalo() {
+        return this.palo.ordinal();
+    }
+
+    public Palo getPalo() {
+        return this.palo;
+    }
+
+    public Valor getValor() {
+        return this.valor;
     }
 
 
