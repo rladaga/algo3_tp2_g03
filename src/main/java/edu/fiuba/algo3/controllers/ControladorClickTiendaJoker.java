1978e5a369f2ebecd19e1a4556aa13a788347e68
agustindetoffoli <adetoffoli@fi.uba.ar>
2024-12-09 23:30:48 -0300

    Agregamos controladores de la tienda
    Co-authored-by: rladaga <rladaga@fi.uba.ar>
    Co-authored-by: tomasapud <tapud@fi.uba.ar>
    Co-authored-by: alvarezanibal <analvarez@fi.uba.ar>

---
+++ src/main/java/edu/fiuba/algo3/controllers/ControladorClickTiendaJoker.java
@@ -0,0 +1,39 @@
+package edu.fiuba.algo3.controllers;
+
+import edu.fiuba.algo3.modelo.Carta.Carta;
+import edu.fiuba.algo3.modelo.*;
+import edu.fiuba.algo3.modelo.Joker.Joker;
+import edu.fiuba.algo3.modelo.Tarot.Tarot;
+import javafx.event.EventHandler;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class ControladorClickTiendaJoker implements EventHandler<MouseEvent> {
+    private final ImageView cartaImagen;
+    private final boolean[] estaSeleccionada;
+    private final int[] contador;
+    private final Joker joker;
+    private final Balatro modelo;
+
+    public ControladorClickTiendaJoker(ImageView cartaImagen, boolean[] estaSeleccionada, int[] contador, Joker joker, Balatro modelo) {
+        this.cartaImagen = cartaImagen;
+        this.estaSeleccionada = estaSeleccionada;
+        this.contador = contador;
+        this.joker = joker;
+        this.modelo = modelo;
+    }
+
+    @Override
+    public void handle(MouseEvent event) {
+        if (!estaSeleccionada[0] && contador[0] == 0) {
+            estaSeleccionada[0] = !estaSeleccionada[0];
+            cartaImagen.setScaleX(1.2);
+            cartaImagen.setScaleY(1.2);
+            contador[0]++;
+        } else if (estaSeleccionada[0]){
+            cartaImagen.setScaleX(1.0);
+            cartaImagen.setScaleY(1.0);
+            contador[0]--;
+        }
+    }
+}
